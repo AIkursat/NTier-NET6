cc359f0c5c9121ba881fa877f337f30929c15b7e
AIKursat <aynurkursat1999@hotmail.com>
2022-02-18 22:14:34 +0300

    Create Entities

---
+++ NTier.Core/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Core
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; } // Category has one more products. Note: List is other option to use.
+    }
+}
---
+++ NTier.Core/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Core
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; } // Because every products have one category. Note: It's name navigation property.
+        public ProductFeature ProductFeature { get; set; }
+
+    }
+}
