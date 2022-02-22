18b9bc66c286e5ca3cadc6bb9787e5afc95da36d
AIKursat <aynurkursat1999@hotmail.com>
2022-02-22 11:58:22 +0300

    Create IService

---
+++ NTier.Core/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Core
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        // Expressions point the delegations.
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task Update(T entity); // bcz it will reflect that changes to db that's why we changed that to async.
+        Task Remove(T entity);
+        Task RemoveRange(T entity);
+    }
+}
