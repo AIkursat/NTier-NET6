c166bc0df61bfdd795774807dc38124bd2ce8571
AIKursat <aynurkursat1999@hotmail.com>
2022-02-22 17:37:42 +0300

    Create DbContext

---
+++ NTier.Repository/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTier.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)  : base(options)
+        {
+
+
+        }
+
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<ProductFeature> productFeatures { get; set; } 
+
+
+    }
+}
