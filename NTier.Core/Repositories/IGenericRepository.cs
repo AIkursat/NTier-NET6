8e5dfbda348b8a7d8a5dfdec11d96d00278cd9b4
AIKursat <aynurkursat1999@hotmail.com>
2022-02-22 11:09:56 +0300

    Finish IGenericRepository

---
+++ NTier.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        // Task is always Async
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        // Expressions point the delegations.
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(T entity);
+
+    }
+}
